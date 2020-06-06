396b4a01ad9fc2e275c2241c1c768bf8ed8c1b76
onkararenukar <onkararenukar@gmail.com>
2020-06-06 13:02:54 +0530

    dashboard design

---
+++ Upastith/src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule, MatButton } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatMenuModule } from '@angular/material/menu';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
