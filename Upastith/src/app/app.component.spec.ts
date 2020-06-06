3b4a422246202f502f5074e13508be5d073ea1a8
onkararenukar <onkararenukar@gmail.com>
2020-06-06 10:15:18 +0530

    MATERIALized new

--- Upastith/src/app/app.component.spec.ts
+++ Upastith/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Upastih'`, () => {
+  it(`should have as title 'Upastith'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Upastih');
+    expect(app.title).toEqual('Upastith');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Upastih app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Upastith app is running!');
   });
 });
